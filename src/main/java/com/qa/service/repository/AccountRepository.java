b19428b2602528e99c7fcd037ed05773dd988d8c
Tiffany Yeung <tiffany.yeung@qa.com>
2018-04-29 01:17:34 +0100

    Added constraints to attributes(in Account.java) and method parameter(in AccountRepository.java) to validate data, and added new test case

--- src/main/java/com/qa/service/repository/AccountRepository.java
+++ src/main/java/com/qa/service/repository/AccountRepository.java
@@ -3,6 +3,8 @@ package com.qa.service.repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.validation.constraints.NotNull;
+
 import static javax.transaction.Transactional.TxType.REQUIRED;
 
 import com.qa.persistence.domain.Account;
@@ -16,7 +18,7 @@ public class AccountRepository {
 	// business methods
 	
 	@Transactional(REQUIRED)
-	public Account createAnAccount(Account newAccount) {
+	public Account createAnAccount(@NotNull Account newAccount) {
 		em.persist(newAccount);
 		return newAccount;
 	}
